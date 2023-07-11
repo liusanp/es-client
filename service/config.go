5775bb0c1a191ae6624d74d82ff6b7d2784eb359
1y <1y@52study.ac.cn>
2023-07-11 10:07:02 +0800

    feat:

--- service/config.go
+++ service/config.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"es-client/commons"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -20,5 +21,23 @@ func GetConfig(c *gin.Context) {
 		"code": 200,
 		"msg":  "获取配置成功",
 		"data": conf,
+	})
+}
+
+// SetConfig
+// @Summary 设置es配置
+// @Tags es配置
+// @Accept json
+// @Produce json
+// @Success 200 {string} json{"code","msg","data"}
+// @Router /conf/set [post]
+func SetConfig(c *gin.Context) {
+	// conf := commons.GetConfig("app.es.conf")
+	b, _ := c.Request.GetBody()
+	log.Println(b)
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "设置配置成功",
+		"data": nil,
 	})
 }
